0773f907f754b82880038ab8713c770e431e87e5
Alyssa Bell <alyssa.bell@troyweb.com>
2020-03-07 17:32:28 -0500

    Updates

---
+++ precache-manifest.d213ab47d9f1585b7fb091f5343bb0c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f6903dcf940f96cd516913bf833ad3a",
+    "url": "/retro-board/index.html"
+  },
+  {
+    "revision": "4366be63225768038677",
+    "url": "/retro-board/static/css/main.da6967cc.chunk.css"
+  },
+  {
+    "revision": "708a76bf9c64e2f676ad",
+    "url": "/retro-board/static/js/2.8cd06780.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/retro-board/static/js/2.8cd06780.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4366be63225768038677",
+    "url": "/retro-board/static/js/main.69bb64f3.chunk.js"
+  },
+  {
+    "revision": "a8923f1c46674b7c6ffa",
+    "url": "/retro-board/static/js/runtime-main.638d41e9.js"
+  }
+]);
